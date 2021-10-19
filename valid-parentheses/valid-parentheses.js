20ddc0d87a48455d8ed6a8b7c4147bebde7bcabe
Suzie Kim <suuuzie18@gmail.com>
2021-10-19 13:59:59 +0900

    algorithm

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -2,34 +2,35 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(expr) {
+var isValid = function(s) {
   const stack = [];
 
-  for (let i = 0; i < expr.length; i++) {
-    const x = expr[i];
-
-    if (x == '(' || x == '[' || x == '{') {
-      stack.push(x);
-      continue;
+  for (let element of s) {
+    if (element === '(' || element === '[' || element === '{') {
+      stack.push(element);
     }
 
-    if (stack.length == 0) return false;
+    if (stack.length === 0) return false;
 
-    let check;
-    switch (x) {
+    let lastElement;
+    
+    switch (element) {
       case ')':
-        check = stack.pop();
-        if (check == '{' || check == '[') return false;
+        lastElement = stack.pop();
+        if (lastElement === '{' || lastElement === '[')
+        return false;
         break;
 
       case '}':
-        check = stack.pop();
-        if (check == '(' || check == '[') return false;
+        lastElement = stack.pop();
+        if (lastElement === '(' || lastElement === '[')
+        return false;
         break;
 
       case ']':
-        check = stack.pop();
-        if (check == '(' || check == '{') return false;
+        lastElement = stack.pop();
+        if (lastElement === '(' || lastElement === '{')
+        return false;
         break;
     }
   }
