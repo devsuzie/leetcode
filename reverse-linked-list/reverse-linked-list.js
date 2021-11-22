cdd308de5dacbe706d22202011e88a47d44d25bd
Suzie Kim <40863240+devsuzie@users.noreply.github.com>
2021-11-22 21:54:05 +0900

    Time: 169 ms (5.21%), Space: 40.3 MB (88.38%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let previous = null
+
+  while (head) {
+    [head.next, previous, head] = [previous, head, head.next]
+  }
+
+  return previous
+}
