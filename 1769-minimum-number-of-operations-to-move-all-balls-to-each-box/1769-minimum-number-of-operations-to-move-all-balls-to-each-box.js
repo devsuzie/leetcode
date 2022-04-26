2f2d3164420b622f16985d487700a987e5715020
Suzie Kim <40863240+devsuzie@users.noreply.github.com>
2022-04-26 13:12:11 +0900

    Time: 393 ms (35.38%), Space: 44.4 MB (84.69%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -2,14 +2,16 @@
  * @param {string} boxes
  * @return {number[]}
  */
-var minOperations = function(boxes) {
-    let move = new Array(boxes.length).fill(0)
-    for(let i=0; i<boxes.length; i++){
-        for(let j=0; j<boxes.length; j++){
-            if(boxes[j] == '1'){
-                move[i] += Math.abs(j-i)
-            }
-        }
+var minOperations = function (boxes) {
+  let answer = new Array(boxes.length).fill(0);
+
+  for (let i = 0; i < boxes.length; i++) {
+    for (let j = 0; j < boxes.length; j++) {
+      if (boxes[j] === '1') {
+        answer[i] = answer[i] + Math.abs(j - i);
+      }
     }
-    return move;
+  }
+
+  return answer;
 };
